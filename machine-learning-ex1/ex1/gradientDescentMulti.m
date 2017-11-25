0836ccec6f696c050e4078704a8c8cb725b08906
Yogesh Chauhan <anujsingh432@gmail.com>
2017-11-25 12:35:49 +0530

    gradient descent multivarieate

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,19 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    
+    theta = theta - alpha*( ((X*theta - y)'*X)' )/m;
+    
     % ============================================================
-
+    
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
 
